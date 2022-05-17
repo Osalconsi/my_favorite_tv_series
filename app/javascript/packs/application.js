8ec18c266a1f23c365707380e522f91ca95dc9dc
Osvaldo Contreras <o.contreras2093@gmail.com>
2022-05-16 22:06:34 -0400

    actulización

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'css/site'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/tv_shows', to: 'tv_shows#index'
+  get '/welcome', to: 'pages#welcome'
+    root 'pages#welcome'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
